b4fbee265b8e5342eb0da39eaffaf0552cb362cc
Renato Adriano de Souza <renatounico@hotmail.com>
2020-11-21 14:54:52 -0300

    Subindo para github

---
+++ aposentadoria.js
@@ -0,0 +1,12 @@
+const aspectos = `Silvana`
+const seco = `F`
+const idade = 48
+const contribuicao = 30
+
+
+
+if  ( contribuicao >= 30 ) {
+    console.log (`Seu tempo de contribuição é ${contribuicao}. Você já pode se aposentar.`)
+} else if ( contribuicao < 30 ) {
+    console.log (`Seu tempo de contribuição é ${contribuicao}. Continue trabalhando.`)
+}
